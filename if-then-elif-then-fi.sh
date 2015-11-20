1e01afd4cd13b79b12fc5214a810003aa8b92afb
liuhongbin1991 <374100289@qq.com>
2015-11-20 19:24:48 +0800

    this shell if for a shell to begin with input

---
+++ if-then-elif-then-fi.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#below we test a input
+if  [ -z $1 ]
+then 
+       echo "this shell must choose {checkyc|rs}"
+elif [ $1 = "checkyc" ]
+then
+	pwd && echo "checkyc"
+elif [ $1 = "rs" ]
+then
+	echo "rs"
+fi
