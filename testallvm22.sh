c937c2054058e7074c8e0c9d36c2fea02293588e
liuhongbin1991 <374100289@qq.com>
2015-11-30 18:03:07 +0800

    fix allvmaliip

--- testallvm22.sh
+++ testallvm22.sh
@@ -4,13 +4,24 @@
 #Author: liuhb@dtdream.com
 #Create Date:2015-11-30 11:18
 #Fuction: this script is for users to test all vm's ssh 
-mysql -hecs.mysql.rds.pcloud.ga -P 3022 -prcwpAeak97qPsdfz -uecs houyiregiondb -e "select * from vm where status='running'; " >/tmp/allrunningvm
+houyiregiondb=ecs.mysql.rds.pcloud.ga
+houyiregiondbport=3022
+houyiregiondbuser=ecs
+houyiregiondvpasswd=rcwpAeak97qPsdfz
+mysql -h$houyiregiondb -P$houyiregiondbport -p$houyiregiondvpasswd -uhouyiregiondbuser houyiregiondb -e "select * from vm where status='running'; " >/tmp/allrunningvm
 sed -i '1d' /tmp/allrunningvm
 if [ -f /tmp/allvmrefuse ]
 then 
     > /tmp/allvmrefuse
 else
     touch /tmp/allvmrefuse
+    > /tmp/allvmrefuse
+fi
+if [ -f /tmp/allvmaliip ]
+then
+    > /tmp/allvmaliip
+else
+    touch /tmp/allvmaliip
     > /tmp/allvmrefuse
 fi
 for i in `cat /tmp/allrunningvm`;do go2which $i|grep 'instance_ips' |awk -F '|' '{print $1}' |awk '{print $2,substr($2,4,13)}' |awk '{print $2}' >>/tmp/allvmaliip ;done;
