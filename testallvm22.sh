442fd0560dbde6c53c6c905f35508e04dbedbe5b
liuhongbin1991 <374100289@qq.com>
2015-11-30 16:04:13 +0800

    add 分隔符

--- testallvm22.sh
+++ testallvm22.sh
@@ -14,5 +14,7 @@ else
     > /tmp/allvmrefuse
 fi
 for i in `cat /tmp/allrunningvm`;do go2which $i|grep 'instance_ips' |awk -F '|' '{print $1}' |awk '{print $2,substr($2,4,13)}' |awk '{print $2}' >>/tmp/allvmaliip ;done;
+echo "================"
+date
 for i in `cat /tmp/allvmaliip`;do for (( j = 1; j <= 100; j++));do nc -zvw3 $i 22; done ;done 2>/tmp/allvmrefuse 1>/dev/null
 cat /tmp/allvmrefuse |sort |uniq 
