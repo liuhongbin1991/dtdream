826f29b47f40385b14bad9409c8680f2a5a33677
liuhongbin1991 <374100289@qq.com>
2017-02-13 17:56:39 +0800

    renewed
    支持自定义网段和输出可用IP地址功能

--- scanip/scanip.sh
+++ scanip/scanip.sh
@@ -1,17 +1,23 @@
-#!/bin/bash
-IP=192.168.1
-read -p "network:" network;
-#init IPlist for scan
-IP=`echo $network |awk -F'.' 'BEGIN{OFS="."}{print $1,$2,$3}'`
->./IPlist 
-for ((i=1;i<=255;i++));do echo $IP.$i >>./IPlist;done;
-#echo $network |awk -F'.' 'BEGIN{OFS="."}{print $1,$2,$3}' >IP
-#sed -i '' 's/192.168.1/'$IP'/g' /Users/liuhongbin/GitHub/dtdream/scanip/IPlist
-#IP=$network |awk -F'.' 'BEGIN{OFS="."}{print $1,$2,$3}'
-#echo $IP
-#ping -c $i
-for i in `cat ./IPlist`;
-do if ping -c 1  -W 200  $i |grep time= >/dev/null 2>&1
-	then echo $i
-fi ;
-done;
+#/bin/bash
+#this shell is used for scan all used ip and echo the ips who are useable
+#Time 2017-02-13
+#Auth liu.hongbin@h3c.com
+
+#define a subnet
+read -p "please input a subnet like 192.168.1.0/24:" orgnetwork
+if test -z $orgnetwork
+then
+        echo "error input";
+        exit
+else
+        network=`echo $orgnetwork |awk -F'.' 'BEGIN{OFS="."}{print $1,$2,$3}'`;
+        for ((i=1;i<=254;i++));
+        do
+                if ping -c 1 -w 0.1 $network\.$i |grep "100% packet loss" >/dev/null 2>&1;
+                then
+                        #echo "haha" >/dev/null 2>&1;
+                        echo $network\.$i can be used;
+                fi
+
+        done;
+fi
